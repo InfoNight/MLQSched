ac40608214785d7aad75495930eb49d183ccc4ef
Jaejun Lee <jaejun.leee@gmail.com>
2022-06-06 02:33:48 +0000

    chore: generateConfigs for simulation mode

--- run.sh
+++ run.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
 SUFFIX=$1
+mkdir ourResults
 outdir=ourResults/${SUFFIX}
 mkdir $outdir
 
 function generateConfigs {
+    SIM_TYPE=$3
     cp configs/${1}core_base.cfg configs/${1}core_base.cfg.bak
+
     cat configs/${1}core_base.cfg.bak \
+    | sed -E "s/sim_type = [a-zA-Z]+/sim_type = ${SIM_TYPE}/" \
+    > configs/${1}core_base.cfg.bak1
+
+    if [ $SIM_TYPE == "CYCLE" ];
+    then
+        cat configs/${1}core_base.cfg.bak1 \
+        | sed -E 's/\/\/ sim_cycle_max = [0-9]+/sim_cycle_max = 10000000/' \
+        > configs/${1}core_base.cfg.bak2
+    else
+        echo "MOOYAHO"
+        cat configs/${1}core_base.cfg.bak1 \
+        | sed -E 's/sim_cycle_max = [0-9]+/\/\/ sim_cycle_max = 10000000/' \
+        > configs/${1}core_base.cfg.bak2
+    fi
+
+    cat configs/${1}core_base.cfg.bak2 \
     | sed -E "s/sched.sched_algo = [a-zA-Z]+/sched.sched_algo = ${2}/" \
     > configs/${1}core_base_${2}.cfg
+
+    rm configs/${1}core_base.cfg.bak1
+    rm configs/${1}core_base.cfg.bak2
 }
 
 ###########################################
@@ -16,12 +38,13 @@ function generateConfigs {
 
 # ** Applications: forkset, bootup, tpcc64, libquantum
 workloads=("NONE" "HHHH" "HHHL" "HHLL" "HLLL" "LLLL")
-for sched in "FRFCFS" "FCFS" "ATLAS" "BLISS" "TCM" "PARBS" "MLQSched"
-# for sched in "MLQSched"
+# for sched in "FRFCFS" "FCFS" "ATLAS" "BLISS" "TCM" "PARBS" "MLQSched"
+for sched in "MLQSched"
 do
-    for core in 1
+    for core in 16 32
     do
-        # generateConfigs $core $sched
+        # generateConfigs $core $sched CYCLE
+
         if [ ${core} -eq 1 ];
         then
             mono bin/sim.exe -output ${outdir}/${core}core_base_L_${sched}_.json -config configs/${core}core_base_$sched.cfg -N $core -workload workloads/${core}core 1
